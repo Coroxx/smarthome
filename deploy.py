ae043b7b16a5807f9b8fff7afae8f22e7dc211e6
Coroxx <unnainstituteur@gmail.com>
2021-07-06 13:07:17 +0200

    Revert "Revert "🚸 Easier to deploy 🚸""
    This reverts commit f062407a7b1446ae7746533b3a67407e4479353e.

--- deploy.py
+++ deploy.py
@@ -4,6 +4,8 @@ import fileinput
 
 tempFile = open("resources/views/home.blade.php", "r+")
 
+host = str(input('Enter your host : (It can be an ip:port, ip etc..) : '))
+
 for line in fileinput.input("resources/views/home.blade.php"):
-    tempFile.write(line.replace(".localhost", ".test"))
+    tempFile.write(line.replace("yourhost.local", host))
 tempFile.close()
