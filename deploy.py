3c78713ecf9a899bfde0863ed8b153f15850d195
Coroxx <unnainstituteur@gmail.com>
2021-07-06 13:30:48 +0200

    🚸 Easier to deploy (Fixed) 🚸

--- deploy.py
+++ deploy.py
@@ -4,8 +4,12 @@ import fileinput
 
 tempFile = open("resources/views/home.blade.php", "r+")
 
-host = str(input('Enter your host : (It can be an ip:port, ip etc..) : '))
+host = str(
+    input(
+        "Enter your host : (It can be an ip:port, ip etc.. but set the http:// or https before) : "
+    )
+)
 
 for line in fileinput.input("resources/views/home.blade.php"):
-    tempFile.write(line.replace("yourhost.local", host))
+    tempFile.write(line.replace("http://smarthome.localhost", host))
 tempFile.close()
