53937a48d5cb09d16d3f9efe4472d770ba2e7c95
Coroxx <unnainstituteur@gmail.com>
2021-07-06 12:58:07 +0200

    🚸 Easier to deploy 🚸

--- windows.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import fileinput
-
-tempFile = open("resources/views/home.blade.php", "r+")
-
-for line in fileinput.input("resources/views/home.blade.php"):
-    tempFile.write(line.replace(".test", ".localhost"))
-tempFile.close()
